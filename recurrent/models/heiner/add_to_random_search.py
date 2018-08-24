3919e1260dbccc5e550126fa0ae6489e682b3b30
Heiner Spiess <spiess.heiner@gmail.com>
2018-08-24 16:50:43 +0200

    manual adding of hcombs to to_run list

--- recurrent/models/heiner/add_to_random_search.py
+++ recurrent/models/heiner/add_to_random_search.py
@@ -1,22 +1,35 @@
 from heiner.hyperparameters import RandomSearch
 import os
 
-metric_used = 'BAC'
-STAGE = 1
-time_steps = 1000
 
-number_of_hcombs = 0
 
-#################################################
 
-rs = RandomSearch(metric_used=metric_used, STAGE=STAGE, time_steps=time_steps)
+################################################# ADD NEW HCOMBS
 
-# TODO: change ranges here
+
+def add_new_hcombs(number_of_hcombs, save_path):
+    metric_used = 'BAC'
+    STAGE = 1
+    time_steps = 1000
+
+    rs = RandomSearch(metric_used=metric_used, STAGE=STAGE, time_steps=time_steps)
+    h = rs._get_hcombs_to_run(number_of_hcombs)
+    rs.save_hcombs_to_run(save_path, number_of_hcombs)
+
+
+def add_hcombs_from_ids(ids, save_path):
+    rs = RandomSearch()
+    rs.add_hcombs_to_run_via_id(ids, save_path)
 
 ################################################# MODEL LOG AND CHECKPOINT SETUP DEPENDENT ON HYPERPARAMETERS
 
-model_name = 'LDNN_v1'
-save_path = os.path.join('/home/spiess/twoears_proj/models/heiner/model_directories', model_name)
-os.makedirs(save_path, exist_ok=True)
+if __name__ == '__main__':
+    model_name = 'LDNN_v1'
+    save_path = os.path.join('/home/spiess/twoears_proj/models/heiner/model_directories', model_name)
+    os.makedirs(save_path, exist_ok=True)
 
-rs.save_hcombs_to_run(save_path, number_of_hcombs)
+    add_hcombs_from_ids(16, save_path)
+
+
+
+
