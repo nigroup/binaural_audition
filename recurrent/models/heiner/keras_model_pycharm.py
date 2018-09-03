565e8913b448d5e76b38736e51f25530920d2316
Heiner Spiess <spiess.heiner@gmail.com>
2018-09-03 17:32:11 +0200

    small change

--- recurrent/models/heiner/keras_model_pycharm.py
+++ recurrent/models/heiner/keras_model_pycharm.py
@@ -1,8 +1,11 @@
 import heiner.keras_model as keras_model
+import heiner.keras_model_final as keras_model_final
 
 
 if __name__ == '__main__':
     # model_name = 'LDNN_pycharm'
     model_name = 'LDNN_v1'
 
-    keras_model.run_experiment(False, 1, 'BAC', '0', 5, False, 1000, model_name=model_name)
+    # keras_model.run_experiment(False, 1, 'BAC', '0', 5, False, 1000, model_name=model_name)
+
+    keras_model_final.run_final_experiment(False, '3', 15, 1, model_name='LDNN_final', model_name_old='LDNN_v1')
