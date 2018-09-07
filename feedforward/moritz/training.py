d74ee1d9e76d8a2afc373fa455180f1475f576c1
Moritz Augustin <augustin@ni.tu-berlin.de>
2018-09-07 14:36:29 +0200

    fix tried to get bs64 running too

--- feedforward/moritz/training.py
+++ feedforward/moritz/training.py
@@ -222,7 +222,7 @@ import tensorflow as tf
 config = tf.ConfigProto()
 
 if params['batchsize'] == 64:
-    config.gpu_options.per_process_gpu_memory_fraction = 0.4
+    config.gpu_options.per_process_gpu_memory_fraction = 0.399
 elif params['batchsize'] == 128:
     config.gpu_options.per_process_gpu_memory_fraction = 0.6
 else:
